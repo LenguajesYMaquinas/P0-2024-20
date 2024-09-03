fa9f362e1a3226bdc031185e1dd0b775dacadc33
sfrancop <s.francop@uniandes.edu.co>
2024-09-03 08:43:33 -0500

    allowing recursion and using macros in blocks

--- parser.py
+++ parser.py
@@ -148,6 +148,7 @@ def parser(tokens):
     brackets_stack = 0
     
     variables = []
+    macro_parameters_quantity = {}
     
     macro_pending_parameters = False
     macro_recieving_parameters = False
@@ -155,25 +156,46 @@ def parser(tokens):
     macro_pending_block =  False
     macro_in_block = False
     
+    current_macro_in_definition_name = None
+    
+    macro_calling_pending_parameters = False
+    macro_calling_parameters_received = 0
+    macro_calling_name = None
+    
     for token_object in tokens:
         token = token_object.type
         token_value = token_object.value
         #print("{}: {}".format(token, token_object.value))
         
+        # Closing brackets verification
         if token == "LBRACKET":
             brackets_stack += 1
         elif token == "RBRACKET":
             brackets_stack -= 1
             
+        # Not duplicated variables definition verification
         if current_state == "VAR" and token == "VARIABLE":
             if token_value not in variables:
                 variables.append(token_value)
             else:
+                print('a')
                 return False
         
+        # Use of variables already declareds verification
         if current_state == "EQUALS" and token == "VARIABLE" and token_value not in variables and not macro_in_block:
+            print('b')
             return False
         
+        # Not duplicated macros definition verification
+        if current_state == "MACRO" and token == "VARIABLE":
+            if token_value not in macro_parameters_quantity:
+                macro_parameters_quantity[token_value] = 0
+                current_macro_in_definition_name = token_value
+            else:
+                print('c')
+                return False
+        
+        # Use in MACROs of parameters and variables already declared verification
         if token == "MACRO":
             macro_pending_block = True
             macro_pending_parameters = True
@@ -181,6 +203,7 @@ def parser(tokens):
             macro_recieving_parameters = True
         if token == "VARIABLE" and macro_recieving_parameters:
             variables_in_macro.append(token_value)
+            macro_parameters_quantity[current_macro_in_definition_name] += 1
         if token == "RPAREN" and macro_recieving_parameters:
             macro_recieving_parameters = False
             macro_pending_parameters = False
@@ -188,19 +211,45 @@ def parser(tokens):
             macro_in_block = True
         if token == "RBRACKET" and macro_in_block:
             macro_in_block = False
+            macro_pending_block = False
             variables_in_macro = []
+            print(macro_parameters_quantity)
+            current_macro_in_definition_name = None
         if token == "VARIABLE" and macro_in_block:
-            if token_value not in variables_in_macro and token_value not in variables:
+            if token_value not in variables_in_macro and token_value not in variables and token_value not in macro_parameters_quantity:
+                print('d')
                 return False
+            
+        # Allowing using macros in blocks
+        if current_state in ['LBRACKET', 'SEMICOLON'] and token == 'VARIABLE':
+            if token_value not in variables_in_macro and token_value not in variables and token_value not in macro_parameters_quantity:
+                print('e')
+                return False 
+            elif token_value in macro_parameters_quantity:
+                macro_calling_pending_parameters = True
+                macro_calling_name = token_value
+        if current_state in ['LPAREN', 'COMMA'] and token == "VARIABLE" and macro_calling_pending_parameters:
+            macro_calling_parameters_received += 1
+        if current_state == 'VARIABLE' and token == 'RPAREN' and macro_calling_pending_parameters:
+            if macro_calling_parameters_received!= macro_parameters_quantity[macro_calling_name]:
+                print('f')
+                return False
+            macro_calling_pending_parameters = False
+            macro_calling_parameters_received = 0
+            macro_calling_name = None
         
+        # States transition
         token_index = adjacency_matrix_order_inverted[token]
         if adjacency_matrix[current_state_index][token_index]:
             current_state = token
             current_state_index = adjacency_matrix_order_inverted[current_state]
-        else:            
+        else:
+            print(current_state, token_value)
+            print('g')          
             return False
     
     if current_state not in final_states and brackets_stack == 0:
+        print('h')
         return False
 
     return True
